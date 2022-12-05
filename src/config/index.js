d9141a7f52f84d8ada874d49a333b23f1577ffcd
rosemorphy <118449990+rosemorphy@users.noreply.github.com>
2022-12-05 14:47:53 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wdremit.herokuapp.com/api/v1';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://dull-blue-salmon-toga.cyclic.app/api/v1';
 
 // export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "https://wdremitt.netlify.app";
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "https://widremt.netlify.app";
